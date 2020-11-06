a6068f48e43299c2ad9a87de59fc036106ceaa38
FXGstudio <Administrator@pc-201908211654>
2020-11-06 19:09:07 +0800

    1.3.1 update~

---
+++ src/main/java/net/mcbbs/youcanexplainnothing/item/BronzeHammerItem.java
@@ -0,0 +1,66 @@
+
+package net.mcbbs.youcanexplainnothing.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcbbs.youcanexplainnothing.itemgroup.NothingSurfaceItemGroup;
+import net.mcbbs.youcanexplainnothing.YouCanExplainNothingModElements;
+
+@YouCanExplainNothingModElements.ModElement.Tag
+public class BronzeHammerItem extends YouCanExplainNothingModElements.ModElement {
+	@ObjectHolder("you_can_explain_nothing:bronze_hammer")
+	public static final Item block = null;
+	public BronzeHammerItem(YouCanExplainNothingModElements instance) {
+		super(instance, 224);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 300;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(BronzeIngotItem.block, (int) (1)));
+			}
+		}, 1, -3f, new Item.Properties().group(NothingSurfaceItemGroup.tab)) {
+			@Override
+			public boolean hasContainerItem() {
+				return true;
+			}
+
+			@Override
+			public ItemStack getContainerItem(ItemStack itemstack) {
+				ItemStack retval = new ItemStack(this);
+				retval.setDamage(itemstack.getDamage() + 1);
+				if (retval.getDamage() >= retval.getMaxDamage()) {
+					return ItemStack.EMPTY;
+				}
+				return retval;
+			}
+		}.setRegistryName("bronze_hammer"));
+	}
+}
--- src/main/java/net/mcbbs/youcanexplainnothing/procedures/GrinderSimpleUpdateTickProcedure.java
+++ src/main/java/net/mcbbs/youcanexplainnothing/procedures/GrinderSimpleUpdateTickProcedure.java
@@ -8,6 +8,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.item.ItemStack;
 
+import net.mcbbs.youcanexplainnothing.item.ZincIngotItem;
+import net.mcbbs.youcanexplainnothing.item.ZincDustItem;
 import net.mcbbs.youcanexplainnothing.item.TinIngotItem;
 import net.mcbbs.youcanexplainnothing.item.TinDustItem;
 import net.mcbbs.youcanexplainnothing.item.CopperIngotItem;
@@ -185,6 +187,73 @@ public class GrinderSimpleUpdateTickProcedure extends YouCanExplainNothingModEle
 						if (_ent != null) {
 							final int _sltid = (int) (1);
 							final ItemStack _setstack = new ItemStack(TinDustItem.block, (int) (1));
+							_setstack.setCount((int) 2);
+							_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+								if (capability instanceof IItemHandlerModifiable) {
+									((IItemHandlerModifiable) capability).setStackInSlot(_sltid, _setstack);
+								}
+							});
+						}
+					}
+				}
+			}
+			if ((new ItemStack(ZincIngotItem.block, (int) (1)).getItem() == (new Object() {
+				public ItemStack getItemStack(BlockPos pos, int sltid) {
+					AtomicReference<ItemStack> _retval = new AtomicReference<>(ItemStack.EMPTY);
+					TileEntity _ent = world.getTileEntity(pos);
+					if (_ent != null) {
+						_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+							_retval.set(capability.getStackInSlot(sltid).copy());
+						});
+					}
+					return _retval.get();
+				}
+			}.getItemStack(new BlockPos((int) x, (int) y, (int) z), (int) (0))).getItem())) {
+				if (((new Object() {
+					public int getAmount(BlockPos pos, int sltid) {
+						AtomicInteger _retval = new AtomicInteger(0);
+						TileEntity _ent = world.getTileEntity(pos);
+						if (_ent != null) {
+							_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+								_retval.set(capability.getStackInSlot(sltid).getCount());
+							});
+						}
+						return _retval.get();
+					}
+				}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (1))) == 0)) {
+					{
+						TileEntity _ent = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+						if (_ent != null) {
+							final int _sltid = (int) (0);
+							final int _amount = (int) 1;
+							_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+								if (capability instanceof IItemHandlerModifiable) {
+									ItemStack _stk = capability.getStackInSlot(_sltid).copy();
+									_stk.shrink(_amount);
+									((IItemHandlerModifiable) capability).setStackInSlot(_sltid, _stk);
+								}
+							});
+						}
+					}
+					{
+						TileEntity _ent = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+						if (_ent != null) {
+							final int _sltid = (int) (4);
+							final int _amount = (int) 1;
+							_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+								if (capability instanceof IItemHandlerModifiable) {
+									ItemStack _stk = capability.getStackInSlot(_sltid).copy();
+									_stk.shrink(_amount);
+									((IItemHandlerModifiable) capability).setStackInSlot(_sltid, _stk);
+								}
+							});
+						}
+					}
+					{
+						TileEntity _ent = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+						if (_ent != null) {
+							final int _sltid = (int) (1);
+							final ItemStack _setstack = new ItemStack(ZincDustItem.block, (int) (1));
 							_setstack.setCount((int) 2);
 							_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
 								if (capability instanceof IItemHandlerModifiable) {
